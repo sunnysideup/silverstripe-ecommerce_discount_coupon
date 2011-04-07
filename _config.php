21a76bf5383a15bd3807e5982b8a2725160a467d
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-04-07 13:31:53 +0000

    adding ecommerce_discount_coupon module
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_discount_coupon/trunk@1033 950b781c-c544-efab-2f99-90083ee69683

---
+++ _config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/**
+ * developed by www.sunnysideup.co.nz
+ * author: Nicolaas - modules [at] sunnysideup.co.nz
+**/
+Director::addRules(50, array(
+	DiscountCouponModifier_AjaxController::get_url_segment().'//$Action/$ID/$OtherID' => 'DiscountCouponModifier_AjaxController'
+));
+
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+//===================---------------- START ecommerce_discount_coupon MODULE ----------------===================
+//NOTE: add http://svn.gpmd.net/svn/open/multiselectfield/tags/0.2/ for nicer interface
+//DiscountCoupon::set_form_header("Delivery Option (REQUIRED)");
+//ProductsAndGroupsModelAdmin::add_managed_model("DiscountCouponOptions");
+//===================---------------- END ecommerce_discount_coupon MODULE ----------------===================
