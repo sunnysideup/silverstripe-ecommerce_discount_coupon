2a2c585157290efe198a0b9b6d1f1b51cf220b25
Nicolaas <github@sunnysideup.co.nz>
2011-04-07 13:31:53 +0000

    adding ecommerce_discount_coupon module
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_discount_coupon/trunk@1033 950b781c-c544-efab-2f99-90083ee69683

---
+++ code/model/DiscountCouponOption.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ *@author nicolaas [at] sunnysideup.co.nz
+ *
+ **/
+
+class DiscountCouponOption extends DataObject {
+
+	static $db = array(
+		"Code" => "Varchar(25)",
+		"StartDate" => "Date",
+		"EndDate" => "Date",
+		"DiscountAbsolute" => "Currency",
+		"DiscountPercentage" => "Decimal(4,2)",
+		"CanOnlyBeUsedOnce" => "Boolean"
+	);
+
+	public static $casting = array(
+		"UseCount" => "Int",
+		"IsValid" => "Boolean"
+	);
+
+	public static $searchable_fields = array(
+		"Code"
+	);
+
+	public static $field_labels = array(
+		"DiscountAbsolute" => "Discount as absolute reduction for code - if any (e.g. $10.00)",
+		"DiscountPercentage" => "Discount as percentage of total - if any (e.g. 10%)",
+		"UseCount" => "number of times the code has been used"
+	);
+
+	public static $summary_fields = array(
+		"Code",
+		"StartDate",
+		"EndDate"
+	);
+
+	protected static $coupons_can_only_be_used_once = "";
+		static function set_coupons_can_only_be_used_once($b) {self::$coupons_can_only_be_used_once = $b;}
+		static function get_coupons_can_only_be_used_once() {return self::$coupons_can_only_be_used_once;}
+
+	public static $singular_name = "Discount Coupon Option";
+
+	public static $plural_name = "Discount Coupon Options";
+
+	public static $default_sort = "EndDate DESC, StartDate DESC";
+
+	function UseCount() {
+		$objects = DataObject::get("DiscountCouponModifier", "\"DiscountCouponOptionID\" = ".$this->ID);
+		if($objects) {
+			return $objects->count();
+		}
+		return 0;
+	}
+
+	function IsValid() {
+		if($this->UseCount() > 0 && $this->CanOnlyBeUsedOnce) {
+			return false;
+		}
+		$startDate = strtotime($this->StartDate);
+		$endDate = strtotime($this->EndDate);
+		$today = strtotime("today");
+		$yesterday = strtotime("yesterday");
+		if($startDate < $today && $endDate > $yesterday) {
+			return true;
+		}
+		return false;
+	}
+
+
+	function canDelete($member = null) {
+		return $this->canEdit($member);
+	}
+
+	function canEdit($member = null) {
+		if($this->UseCount()) {
+			return false;
+		}
+		return true;
+	}
+
+	function getCMSFields() {
+		$fields = parent::getCMSFields();
+		return $fields;
+	}
+
+	function onBeforeWrite() {
+		$this->Code = eregi_replace("[^[:alnum:]]", " ", $this->Code );
+		$this->Code = trim(eregi_replace(" +", "", $this->Code));
+		parent::onBeforeWrite();
+	}
+
+}
+
---
+++ code/modifiers/DiscountCouponModifier.php
@@ -0,0 +1,263 @@
+<?php
+
+/**
+ * @author Nicolaas [at] sunnysideup.co.nz
+ * @package: ecommerce
+ * @sub-package: ecommerce_delivery
+ * @description: Shipping calculation scheme based on SimpleShippingModifier.
+ * It lets you set fixed shipping costs, or a fixed
+ * cost for each region you're delivering to.
+ */
+class DiscountCouponModifier extends OrderModifier {
+
+// ######################################## *** model defining static variables (e.g. $db, $has_one)
+
+	public static $db = array(
+		'DebugString' => 'HTMLText',
+		'SubTotalAmount' => 'Currency'
+	);
+
+	public static $has_one = array(
+		"DiscountCouponOption" => "DiscountCouponOption"
+	);
+
+	public static $defaults = array("Type" => "Deductable");
+
+// ######################################## *** cms variables + functions (e.g. getCMSFields, $searchableFields)
+
+	function getCMSFields() {
+		$fields = parent::getCMSFields();
+		return $fields;
+	}
+
+	public static $singular_name = "Discount Coupon Reduction";
+
+	public static $plural_name = "Discount Coupon Reductions";
+
+// ######################################## *** other (non) static variables (e.g. protected static $special_name_for_something, protected $order)
+
+	protected static $actual_deductions = 0;
+
+	protected $debugMessage = "";
+
+// ######################################## *** CRUD functions (e.g. canEdit)
+// ######################################## *** init and update functions
+
+
+
+	public  function setCoupon($discountCoupon) {
+		$this->DiscountCouponOptionID = $discountCoupon->ID;
+		$this->write();
+	}
+
+
+	public  function setCouponByID($discountCouponID) {
+		$this->DiscountCouponOptionID = $discountCouponID;
+		$this->write();
+	}
+
+	public function runUpdate() {
+		$this->checkField("SubTotalAmount");
+		$this->checkField("DiscountCouponOptionID");
+		parent::runUpdate();
+	}
+
+
+
+// ######################################## *** form functions (e. g. showform and getform)
+
+
+	public function showForm() {
+		return $this->Order()->Items();
+	}
+
+	function getForm($controller) {
+		Requirements::themedCSS("DiscountCouponModifier");
+		Requirements::javascript(THIRDPARTY_DIR."/jquery/jquery.js");
+		Requirements::javascript(THIRDPARTY_DIR."/jquery-form/jquery.form.js");
+		Requirements::javascript("ecommerce_discount_coupon/javascript/DiscountCouponModifier.js");
+		$fields = new FieldSet();
+		$fields->push(new TextField('DiscountCouponCode',_t("DiscountCouponModifier.COUPON", 'Coupon')));
+		$actions = new FieldSet(new FormAction_WithoutLabel('processOrderModifier_discountcoupon', _t("DiscountCouponModifier.APPLY", 'Apply')));
+		$controller = new DiscountCouponModifier_AjaxController();
+		$validator = null;
+		return new DiscountCouponModifier_Form($controller, 'ModifierForm', $fields, $actions, $validator);
+	}
+
+
+// ######################################## *** template functions (e.g. ShowInTable, TableTitle, etc...) ... USES DB VALUES
+
+	public function ShowInTable() {
+		return true;
+	}
+	public function CanRemove() {
+		return false;
+	}
+	public function TableValue() {
+		return $this->Amount;
+	}
+	public function TableTitle() {
+		return $this->Name;
+	}
+
+// ######################################## ***  inner calculations.... USES CALCULATED VALUES
+
+// ######################################## *** calculate database fields: protected function Live[field name]  ... USES CALCULATED VALUES
+
+
+	/**
+	*@return int
+	**/
+	protected function LiveName() {
+		$coupon = $this->LiveDiscountCouponOption();
+		if($coupon) {
+			return _t("DiscountCouponModifier.COUPON", "Coupon: ").$coupon->Code;
+		}
+		return "";
+	}
+
+	/**
+	*@return int
+	**/
+	protected function LiveDiscountCouponOptionID() {
+		return $this->DiscountCouponOptionID;
+	}
+
+	/**
+	*@return DiscountCouponOption
+	**/
+	protected function LiveDiscountCouponOption() {
+		if($id = $this->LiveDiscountCouponOptionID()){
+			return DataObject::get_by_id("DiscountCouponOption", $id);
+		}
+	}
+
+
+	/**
+	*@return float
+	**/
+
+	protected function LiveSubTotalAmount() {
+		$order = $this->Order();
+		return $order->SubTotal();
+	}
+
+	/**
+	*@return float
+	**/
+
+	protected function LiveAmount() {
+		if(!self::$actual_deductions) {
+			self::$actual_deductions = 0;
+			$subTotal = $this->LiveSubTotalAmount();
+			if($obj = $this->LiveDiscountCouponOption()) {
+				if($obj->DiscountAbsolute) {
+					self::$actual_deductions += $obj->DiscountAbsolute;
+					$this->debugMessage .= "<hr />usign absolutes for coupon discount: ".self::$actual_deductions;
+				}
+				if($obj->DiscountPercentage) {
+					self::$actual_deductions += ($obj->DiscountPercentage / 100) * $subTotal;
+					$this->debugMessage .= "<hr />usign percentages for coupon discount: ".self::$actual_deductions;
+				}
+			}
+			if($subTotal < self::$actual_deductions) {
+				self::$actual_deductions = $subTotal;
+			}
+			$this->debugMessage .= "<hr />final score: ".self::$actual_deductions;
+			if(isset($_GET["debug"])) {
+				print_r($this->debugMessage);
+			}
+		}
+		return self::$actual_deductions;
+	}
+
+
+	protected function LiveDebugString() {
+		return $this->debugMessage;
+	}
+
+
+// ######################################## *** Type Functions (IsChargeable, IsDeductable, IsNoChange, IsRemoved)
+
+	protected function IsDeductable() {
+		return true;
+	}
+
+// ######################################## *** standard database related functions (e.g. onBeforeWrite, onAfterWrite, etc...)
+
+	function onBeforeWrite() {
+		parent::onBeforeWrite();
+	}
+
+	function requireDefaultRecords() {
+		parent::requireDefaultRecords();
+	}
+
+// ######################################## *** AJAX related functions
+// ######################################## *** debug functions
+
+}
+
+class DiscountCouponModifier_Form extends OrderModifierForm {
+
+	public function processOrderModifier_discountcoupon($data, $form) {
+		if(isset($data['DiscountCouponCode'])) {
+			$newOption = Convert::raw2sql($request['DiscountCouponCode']);
+			$discountCoupon = DataObject::get_one("DiscountCouponOption", "\"Code\" = '".($newOption)."'");
+			if($discountCoupon && $discountCoupon->IsValid()) {
+				$order = ShoppingCart::current_order();
+				$modifiers = $order->Modifiers();
+				if($modifiers) {
+					foreach($modifiers as $modifier) {
+						if ($modifier InstanceOf DiscountCouponModifier) {
+							$modifier->setCoupon($discountCoupon);
+						}
+					}
+				}
+			}
+		}
+		Order::save_current_order();
+		if(Director::is_ajax()) {
+			return ShoppingCart_Controller::json_code();
+		}
+		else {
+			Director::redirect(CheckoutPage::find_link());
+		}
+		return;
+	}
+}
+
+class DiscountCouponModifier_AjaxController extends Controller {
+
+	protected static $url_segment = "discountcouponmodifier";
+		static function set_url_segment($s) {self::$url_segment = $s;}
+		static function get_url_segment() {return self::$url_segment;}
+
+	function ModifierForm($request) {
+		if(isset($request['DiscountCouponCode'])) {
+			$newOption = Convert::raw2sql($request['DiscountCouponCode']);
+			$discountCoupon = DataObject::get_one("DiscountCouponOption", "\"Code\" = '".($newOption)."'");
+			if($discountCoupon && $discountCoupon->IsValid()) {
+				$order = ShoppingCart::current_order();
+				$modifiers = $order->Modifiers();
+				if($modifiers) {
+					foreach($modifiers as $modifier) {
+						if ($modifier InstanceOf DiscountCouponModifier) {
+							$modifier->setCoupon($discountCoupon);
+							$modifier->runUpdate();
+							return ShoppingCart::return_message("success", _t("DiscountCouponModifier.UPDATED", "Coupon Code updated"));
+						}
+					}
+				}
+			}
+		}
+		$modifier->runUpdate();
+		return ShoppingCart::return_message("failure", _t("DiscountCouponModifier.UPDATED", "Coupon Code could NOT be updated"));
+	}
+
+	function Link() {
+		return self::get_url_segment();
+	}
+
+
+}
