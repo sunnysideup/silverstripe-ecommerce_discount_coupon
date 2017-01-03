b668f463a9c98da3d6d9dd78fd2c365223cf0c8c
sunnysideup <sunnysideupdevs@gmail.com>
2017-01-03 13:07:17 +1300

    PATCH: module clean-up

--- tests/EcommerceDiscountCouponTest.php
+++ tests/EcommerceDiscountCouponTest.php
@@ -1,13 +1,12 @@
 <?php
-class EcommerceDiscountCouponTest extends SapphireTest {
+class EcommerceDiscountCouponTest extends SapphireTest
+{
+    protected $usesDatabase = false;
 
-	protected $usesDatabase = false;
+    protected $requiredExtensions = array();
 
-	protected $requiredExtensions = array();
-
-	public function testMyMethod() {
-		$this->assertEquals(1, 1);
-	}
-
+    public function testMyMethod()
+    {
+        $this->assertEquals(1, 1);
+    }
 }
-
